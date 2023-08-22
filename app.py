29b7b9881c6e386d96cb02e301fd54de4707ad9b
Saptarshi <bhattacharyasaptarshi2001@gmail.com>
2023-08-22 14:13:25 +0530

    note detected

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pyaudio
 import wave
 import numpy as np
+import aubio  # Add this import for aubio
+import time
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -9,13 +11,12 @@ RATE = 48000
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
-DETECTION_THRESHOLD = 8  # Adjust this threshold based on your input signal
-DETECTION_WINDOW = 5  # Number of consecutive chunks for detection
+DETECTION_THRESHOLD = 8
+DETECTION_WINDOW = 5
 
 p = pyaudio.PyAudio()
 
-# use device number 2
-
+# Use device number 2
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
@@ -23,24 +24,41 @@ stream = p.open(format=FORMAT,
                 input=True,
                 frames_per_buffer=CHUNK)
 
+# Initialize Aubio pitch detection
+tolerance = 0.8
+win_s = 4096
+hop_s = CHUNK
+samplerate = RATE
+pitch_o = aubio.pitch("default", win_s, hop_s, samplerate)
+pitch_o.set_unit("midi")
+pitch_o.set_tolerance(tolerance)
+
 print("* recording")
 
 frames = []
 chunk_queue = []
 
-while(True):
-    # signal is only detected if it is above the threshold for all samples for DETECTION_WINDOW chunks
-    
-    # get chunks
-    chunk = stream.read(CHUNK)
-    amplitude = np.max(np.abs(np.frombuffer(chunk, dtype=np.int16)))
-    print(amplitude)
-    chunk_queue.append(amplitude)
-    if(len(chunk_queue) > DETECTION_WINDOW):
-        chunk_queue.pop(0)
-    if all(i > DETECTION_THRESHOLD for i in chunk_queue):
-        print("Note detected")
-    
+# start an interval every 0.1s calling get_pitch
+
+while True:
+    try:
+        chunk = stream.read(CHUNK)
+        amplitude = np.max(np.abs(np.frombuffer(chunk, dtype=np.int16)))
+        chunk_queue.append(amplitude)
+        if len(chunk_queue) > DETECTION_WINDOW:
+            chunk_queue.pop(0)
+        if all(i > DETECTION_THRESHOLD for i in chunk_queue):
+            # Use Aubio pitch detection
+            signal = np.frombuffer(chunk, dtype=np.float32).astype(np.float32) / 32768.0
+            pitch = pitch_o(signal)[0]
+            confidence = pitch_o.get_confidence()
+
+            print("Aubio Pitch: {} / Confidence: {}".format(pitch, confidence))
+
+
+    except KeyboardInterrupt:
+        print("* Ctrl+C pressed, exiting")
+        break
 
 print("* done recording")
 
